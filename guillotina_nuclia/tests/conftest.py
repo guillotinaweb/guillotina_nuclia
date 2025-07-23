4d0f3ea4361fe0da5d871f505f216ad943d52b84
Nil <n.bacardit@iskra.cat>
2025-07-23 17:43:06 +0200

    Adding guillotina nuclia (#1)
    * first commit. Adding api calls and chat interfaces

---
+++ guillotina_nuclia/tests/conftest.py
@@ -0,0 +1,6 @@
+pytest_plugins = [
+    "pytest_docker_fixtures",
+    "guillotina.tests.fixtures",
+    "guillotina_nuclia.tests.fixtures",
+    "nuclia.tests.fixtures",
+]
---
+++ guillotina_nuclia/tests/test_content.py
@@ -0,0 +1,76 @@
+import json
+import pytest
+
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_addon(guillotina):
+    response, status = await guillotina(
+        "POST", "/db/guillotina/@addons", data=json.dumps({"id": "nuclia"})
+    )
+    assert status == 200
+    response, status = await guillotina("GET", "/db/guillotina/chats")
+    assert status == 200
+
+    response, status = await guillotina(
+        "POST",
+        "/db/guillotina/chats",
+        data=json.dumps({"@type": "Chat", "id": "foo_chat"}),
+    )
+    assert status == 201
+
+
+# Tetsing needs NUA key
+async def _test_api(guillotina):
+    response, status = await guillotina(
+        "POST",
+        "/db/guillotina/chats",
+        data=json.dumps({"@type": "Chat", "id": "foo_chat"}),
+    )
+    assert status == 201
+    response, status = await guillotina(
+        "POST",
+        "/db/guillotina/chats/foo_chat/@NucliaPredict",
+        data=json.dumps({"question": "Foo question"}),
+    )
+    assert status == 200
+
+    response, status = await guillotina("GET", "/db/guillotina/chats/foo_chat/")
+    assert status == 200
+    assert response["history"] == [
+        {"author": "USER", "text": "Foo question"},
+        {"author": "NUCLIA", "text": "Not enough data to answer this."},
+    ]
+    assert response["responses"] == ["Not enough data to answer this."]
+
+    response, status = await guillotina(
+        "POST",
+        "/db/guillotina/chats/foo_chat/@NucliaPredict",
+        data=json.dumps({"question": "Foo question 2"}),
+    )
+    assert status == 200
+    response, status = await guillotina("GET", "/db/guillotina/chats/foo_chat/")
+    assert response["history"] == [
+        {"author": "USER", "text": "Foo question"},
+        {"author": "NUCLIA", "text": "Not enough data to answer this."},
+        {"author": "USER", "text": "Foo question 2"},
+        {"author": "NUCLIA", "text": "Not enough data to answer this."},
+    ]
+    response, status = await guillotina(
+        "GET", "/db/guillotina/@NucliaAsk?question=Foo question"
+    )
+    assert status == 200
+    assert response == "Not enough data to answer this."
+
+    response, status = await guillotina(
+        "GET", "/db/guillotina/@NucliaSearch?question=Foo question"
+    )
+    assert status == 200
+    assert response == []
+
+    response, status = await guillotina(
+        "GET", "/db/guillotina/@NucliaFind?question=Foo question"
+    )
+    assert status == 200
+    assert len(response) == 15
